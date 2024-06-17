12227dfcc0a37ad3a3ab8299367e05c192c2f963
valentin.v.dimitrov <valentin.v.dimitrov@egt.com>
2024-06-17 14:31:33 +0300

    Printing the data to the main page, not all data included.

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask
+import requests
+import re
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+    site = "https://ibex.bg/%d0%b4%d0%b0%d0%bd%d0%bd%d0%b8-%d0%b7%d0%b0-%d0%bf%d0%b0%d0%b7%d0%b0%d1%80%d0%b0/%d0%bf%d0%b0%d0%b7%d0%b0%d1%80%d0%b5%d0%bd-%d1%81%d0%b5%d0%b3%d0%bc%d0%b5%d0%bd%d1%82-%d0%b4%d0%b5%d0%bd-%d0%bd%d0%b0%d0%bf%d1%80%d0%b5%d0%b4/day-ahead-prices-and-volumes-v2-0/"
+    request = requests.get(site, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+
+    if request.status_code == 200:
+
+        pattern = r"date_default_timezone_set:.*?<tbody>.*?</tbody>"
+
+        match = re.search(pattern, request.text, re.DOTALL)
+
+        if match:
+            specific_line = match.group()
+            print(f"Specific line found: {specific_line}")
+        else:
+            print("No matching line found.")
+    else:
+        print(f"Failed to retrieve the content. Status code: {request.status_code}")
+    return specific_line
 
 
 if __name__ == "__main__":
